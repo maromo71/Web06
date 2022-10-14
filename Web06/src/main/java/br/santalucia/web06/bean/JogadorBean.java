e0cb0600a2fc5d6efc041018b71882bec94620ad
Maromo <professormoraes@gmail.com>
2022-10-13 21:59:33 -0300

    Aula do dia 13 de outubro

---
+++ Web06/src/main/java/br/santalucia/web06/bean/JogadorBean.java
@@ -0,0 +1,78 @@
+package br.santalucia.web06.bean;
+
+
+import br.santalucia.web06.dao.JogadorDao;
+import br.santalucia.web06.model.Jogador;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.util.List;
+
+@ManagedBean
+@SessionScoped
+public class JogadorBean {
+    private Jogador jogador;
+    private JogadorDao dao = new JogadorDao();
+
+
+    @PostConstruct
+    public void init() {
+        jogador = new Jogador();
+    }
+
+    public Jogador getJogador() {
+
+        return jogador;
+    }
+
+    public List<Jogador> getJogadores() {
+        return dao.getJogadores();
+    }
+
+    public String adicionar() {
+        dao.adicionar(jogador);
+
+        jogador = new Jogador();
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null,
+                new FacesMessage("Jogador adicionado com sucesso!"));
+        context.getExternalContext().getFlash().setKeepMessages(true);
+        return "home";
+    }
+
+    public String editar() {
+        dao.editar(jogador);
+
+        jogador = new Jogador();
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Jogador editado com sucesso!"));
+        context.getExternalContext().getFlash().setKeepMessages(true);
+
+        return "home";
+    }
+
+    public String remover() {
+        dao.remover(jogador);
+
+        jogador = new Jogador();
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Jogador removido com sucesso!"));
+        context.getExternalContext().getFlash().setKeepMessages(true);
+
+        return "home";
+    }
+    public void jogadorPorId() {
+        jogador = dao.buscar(Integer.parseInt(jogador.getIdJogador().toString()));
+        if (jogador == null || jogador.getIdJogador()== 0) {
+            jogador = new Jogador();
+            FacesMessage message = new FacesMessage("Jogador não encontrado.");
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+}
